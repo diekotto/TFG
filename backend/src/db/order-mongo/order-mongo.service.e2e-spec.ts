bc3afad6bfe2671c7743190f7e40cf0d0ed5891f
Diego Maroto <diego.diemg@gmail.com>
2020-12-20 18:33:42 +0100

    Orders integration tests

--- backend/src/db/order-mongo/order-mongo.service.e2e-spec.ts
+++ backend/src/db/order-mongo/order-mongo.service.e2e-spec.ts
@@ -1,15 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrderMongoService } from './order-mongo.service';
+import { orderProviders } from './order.providers';
+import { ProvidersModule } from '../providers/providers.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from '../../config/configuration';
+import { Mongoose } from 'mongoose';
 
 describe('OrderMongoService (e2e)', () => {
   let service: OrderMongoService;
+  let connection: Mongoose;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OrderMongoService],
+      imports: [
+        ConfigModule.forRoot({
+          load: [configuration],
+        }),
+        ProvidersModule,
+      ],
+      providers: [OrderMongoService, ...orderProviders],
     }).compile();
 
+    connection = module.get<'MONGODB_CONNECTION'>('MONGODB_CONNECTION') as any;
     service = module.get<OrderMongoService>(OrderMongoService);
+  });
+
+  afterAll(async () => {
+    await connection.connection.close();
   });
 
   it('should be defined', () => {
